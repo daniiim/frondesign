a9615648959c3a64fc828441ae37dffc92141824
“danilomerea” <danilomerea@hotmail.com>
2018-02-12 21:07:51 +0100

    Filter rozefoto's in HTML,CSS, JS
    dmv classes classes in html, display none/block van deze classes en js queryselector i.c.m eventlistener en classList add/remove

--- js/script.js
+++ js/script.js
@@ -1,4 +1,7 @@
 // variabelen gebruiken
+var rozeFilter = document.querySelector('.rozefilter');
+var rozeResults = document.querySelectorAll('.rozeresult');
+var allResults = document.querySelectorAll('.gallery');
 var filterToggle = document.querySelector('.toggle');
 var dropDown = document.querySelector('.dropdown-menu');
 
@@ -7,3 +10,16 @@ filterToggle.addEventListener("click",function(){
     dropDown.classList.toggle('active');  
   }
 );   
+
+// filteren op roze
+rozeFilter.addEventListener("click", function(){
+allResults.forEach(function(el) {
+  el.classList.remove('active');
+});   
+
+rozeResults.forEach(function(el) {
+  el.classList.add('active');
+});   
+});
+//log    
+console.log(allResults);
