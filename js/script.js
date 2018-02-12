e88894a0421a19540947e8d84fa21d05d8202d05
“danilomerea” <danilomerea@hotmail.com>
2018-02-12 21:15:35 +0100

    Filters toegepast in JS van overige kleuren blauw en geel

--- js/script.js
+++ js/script.js
@@ -1,6 +1,10 @@
 // variabelen gebruiken
 var rozeFilter = document.querySelector('.rozefilter');
 var rozeResults = document.querySelectorAll('.rozeresult');
+var blauweFilter = document.querySelector('.blauwefilter');
+var blauweResults = document.querySelectorAll('.blauwresult');
+var geleFilter = document.querySelector('.gelefilter');
+var geelResults = document.querySelectorAll('.geelresult');
 var allResults = document.querySelectorAll('.gallery');
 var filterToggle = document.querySelector('.toggle');
 var dropDown = document.querySelector('.dropdown-menu');
@@ -21,5 +25,28 @@ rozeResults.forEach(function(el) {
   el.classList.add('active');
 });   
 });
+
+// filteren op blauw
+blauweFilter.addEventListener("click", function(){
+allResults.forEach(function(el) {
+  el.classList.remove('active');
+});
+
+blauweResults.forEach(function(el) {
+  el.classList.add('active');
+});
+});
+
+// filteren op geel
+geleFilter.addEventListener("click", function(){
+allResults.forEach(function(el) {
+  el.classList.remove('active');
+});
+
+geelResults.forEach(function(el) {
+  el.classList.add('active');
+});
+});
+
 //log    
 console.log(allResults);
