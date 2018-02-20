e49b862e182b74286592b8392817fe56d66540c6
“danilomerea” <danilomerea@hotmail.com>
2018-02-20 11:24:29 +0100

    new

--- opdracht1/v1.1/js/script.js
+++ opdracht1/v1.1/js/script.js
@@ -1,4 +1,3 @@
-
 // variabelen gebruiken
 var rozeFilter = document.querySelector('.rozefilter');
 var rozeResults = document.querySelectorAll('.rozeresult');
@@ -10,52 +9,106 @@ var allResults = document.querySelectorAll('.gallery');
 var reset = document.querySelector('.geenfilter');
 var filterToggle = document.querySelector('.toggle');
 var dropDown = document.querySelector('.dropdown-menu');
+var loader = document.querySelector('.loaderr');
+var tag = document.querySelector('.tag');
+var touch = document.querySelector('.popup');
+  
+    // touch
+        tag.addEventListener('touchstart',function(){
+            touch.classList.add('active');
+        });
+ 
+        tag.addEventListener('touchend',function(){
+            touch.classList.remove('active');
+        });
+ 
 
-// dropdown toggle
-filterToggle.addEventListener("click",function(){
-    dropDown.classList.toggle('active');  
-  }
-);   
+    // dropdown toggle
+    filterToggle.addEventListener("click",function(){
+        dropDown.classList.toggle('active');  
+      }
+    );   
 
-// filteren op roze
-rozeFilter.addEventListener("click", function(){
-allResults.forEach(function(el) {
-  el.classList.remove('active');
-});   
 
-rozeResults.forEach(function(el) {
-  el.classList.add('active');
-});   
-});
+    // filteren op roze
+    rozeFilter.addEventListener("click", function(){
+    loader.classList.toggle('active');
+    allResults.forEach(function(el) {
+      el.classList.remove('active');
+    });
+        dropDown.classList.toggle('active');
 
-// filteren op blauw
-blauweFilter.addEventListener("click", function(){
-allResults.forEach(function(el) {
-  el.classList.remove('active');
-});
+        setTimeout(function(){
+        loader.classList.toggle('active');
+        }, 1000);
 
-blauweResults.forEach(function(el) {
-  el.classList.add('active');
-});
-});
+    rozeResults.forEach(function(el) {
+    setTimeout(function(){
+      el.classList.add('active');
+    }, 1000);
+    });    
+    });
 
-// filteren op geel
-geleFilter.addEventListener("click", function(){
-allResults.forEach(function(el) {
-  el.classList.remove('active');
-});
 
-geelResults.forEach(function(el) {
-  el.classList.add('active');
-});
-});
+    // filteren op blauw
+    blauweFilter.addEventListener("click", function(){
+    loader.classList.toggle('active');
+        allResults.forEach(function(el) {
+      el.classList.remove('active');
+    });
+        dropDown.classList.toggle('active');
 
-// filters resetten
-reset.addEventListener("click", function() {
-allResults.forEach(function(el) {
-  el.classList.add('active');
-}); 
-});
+        setTimeout(function(){
+        loader.classList.toggle('active');
+        }, 1000);
 
-//log    
-console.log(allResults);
+    blauweResults.forEach(function(el) {
+    setTimeout(function(){
+      el.classList.add('active');
+    }, 1000);
+    });
+    });
+
+
+    // filteren op geel
+    geleFilter.addEventListener("click", function(){
+    loader.classList.toggle('active');
+        allResults.forEach(function(el) {
+      el.classList.remove('active');
+    });
+        dropDown.classList.toggle('active');
+
+        setTimeout(function(){
+        loader.classList.toggle('active');
+        }, 1000);
+
+    geelResults.forEach(function(el) {
+    setTimeout(function(){
+      el.classList.add('active');
+    }, 1000);
+    });
+    });
+
+
+    // filters resetten
+    reset.addEventListener("click", function(){
+    loader.classList.toggle('active');
+        allResults.forEach(function(el) {
+      el.classList.remove('active');
+    });
+        dropDown.classList.toggle('active');
+
+        setTimeout(function(){
+        loader.classList.toggle('active');
+        }, 1000);
+
+    allResults.forEach(function(el) {
+    setTimeout(function(){
+      el.classList.add('active');
+    }, 1000);
+    });
+    });
+
+
+    //log    
+    console.log(allResults);
