cb4ce99a06f5cee475dbdc176ed556936bd0ad33
“danilomerea” <danilomerea@hotmail.com>
2018-03-05 21:38:43 +0100

    slider touch test

--- opdracht2/v1/js/script.js
+++ opdracht2/v1/js/script.js
@@ -27,3 +27,35 @@ vorige.addEventListener('click', function () {
     vorigeSlide();
 });
 
+// swipe 
+function swipedetect(swipe, callback) {
+    var touch = swipe,
+    swiperichting,
+    startX,
+    distX,
+    limit = 100,
+    handleswipe = callback || function(swiperichting) {}
+  
+    touch.addEventListener('touchstart', function(e){
+        var touchobj = e.changedTouches[0]
+        swiperichting = 'Niet'
+        dist = 0
+        startX = touchobj.pageX
+        e.preventDefault()
+    }, false)
+  
+    touch.addEventListener('touchend', function(e){
+        var touchobj = e.changedTouches[0]
+        distX = touchobj.pageX - startX
+            if (Math.abs(distX) >= limit){
+                swiperichting = (distX < 0)? 'Links' : 'Rechts'
+            }
+        handleswipe(swiperichting)
+        e.preventDefault()
+    }, false)
+}
+swipedetect(swipe, function(swiperichting){
+    console.log('' + swiperichting +' geswiped');
+    volgendeSlide();
+});
+
